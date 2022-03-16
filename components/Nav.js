c16d06e76199739bc511832f3ba941f96a25efca
farzadgo <farzadesign@gmail.com>
2022-03-16 11:45:02 +0100

    Nav styling module removed

--- components/Nav.js
+++ components/Nav.js
@@ -1,12 +1,37 @@
 import Link from 'next/link'
-import styles from '../styles/Nav.module.css'
 
 const Nav = () => {
+  const navStyle = {
+    position: 'fixed',
+    minWidth: '100%',
+    height: '60px',
+    padding: '10px',
+    color: '#fff',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: '5',
+    backdropFilter: 'blur(10px)',
+    fontSize: '1.4em'
+  }
+  const navUlStyle = {
+    display: 'flex',
+    color: '#272727',
+    fontWeight: 'bold',
+  }
+  const navLiStyle = {
+    margin: '0 15px'
+  }
+  
   return (
-    <nav className={styles.nav} >
-      <ul>
-        <li><Link href='/'> Cloud Jobs </Link></li>
-        <li><Link href='/upload'> Self Jobs </Link></li>
+    <nav style={navStyle}>
+      <ul style={navUlStyle}>
+        <li style={navLiStyle}>
+          <Link href='/'> Cloud Jobs </Link>
+        </li>
+        <li style={navLiStyle}>
+          <Link href='/upload'> Self Jobs </Link>
+        </li>
       </ul>
     </nav>
   )
